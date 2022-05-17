dc284d6f913c49e37dd00d33f6b04f5ce6a876c0
dgrzeg <dgr.wroc@gmail.com>
2022-05-17 13:29:56 +0200

    row,table components,data,pages

---
+++ components/restaurants/Table.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="table">
+    <h1>TOP 50: THE RANKING</h1>
+    <div class="table-container">
+      <div class="table-col">
+        <RestaurantsRow :isHeader="true" />
+        <RestaurantsRow
+          v-for="(restaurant, index) in restaurantsOrganized.first"
+          :key="restaurant.id"
+          :rank="restaurant.rank"
+          :name="restaurant.name"
+          :index="index"
+          :isHeader="false"
+        />
+      </div>
+      <div class="table-col">
+        <RestaurantsRow :isHeader="true" />
+        <RestaurantsRow
+          v-for="(restaurant, index) in restaurantsOrganized.second"
+          :key="restaurant.id"
+          :rank="restaurant.rank"
+          :name="restaurant.name"
+          :index="index"
+          :isHeader="false"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import restaurants from '@/data.json'
+
+const restaurantsOrganized = {
+  first: [...restaurants].splice(0, 25),
+  second: [...restaurants].splice(25, 25),
+}
+</script>
+
+<style scoped>
+.table {
+  margin: 3rem 0;
+}
+.table h1 {
+  margin-bottom: 2rem;
+}
+.table-container {
+  display: flex;
+  justify-content: space-between;
+}
+.table-col {
+  width: 48%;
+}
+</style>
